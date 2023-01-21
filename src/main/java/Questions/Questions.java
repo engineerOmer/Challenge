9e72da371495cc6edf0d046a8985bf9acc10746b
omerfaruksahin <83252984+engineerOmer@users.noreply.github.com>
2023-01-21 23:45:58 +0300

    Complated

---
+++ src/main/java/Questions/Questions.java
@@ -0,0 +1,54 @@
+package Questions;
+
+public class Questions {
+    /*
+    MVC deseni, 3 katmandan oluşmaktadır ve katmanları birbirinden bağımsız (birbirini etkilemeden) olarak çalışmaktadır.
+    Bu sebeple çoğunlukla büyük çaplı projelerde projelerin yönetiminin ve kontrolünün daha rahat sağlanabilmesi için tercih edilmektedir.
+    MVC ile geliştirilen projelerde projenin detaylarına göre bir çok kişi eş zamanlı olarak kolaylıkla çalışabilmektedir.
+
+    Model Nedir?
+    Verilerin modellendiği genellikle sıradan Java sınflarının(POJO) kullanıldığı bölümdür. Business katmanı modeldedir.
+
+    View nedir?
+    View, MVC’de projenin arayüzlerinin oluşturulduğu bölümdür.
+    Bu bölümde projenin kullanıcılara sunulacak olan HTML dosyaları yer almaktadır.
+
+    Controller nedir?
+    Controller, MVC’de projenin iç süreçlerini kontrol eden bölümdür. Bu bölümde View ile Model arasındaki bağlantı kurulur.
+    Kullanıcılardan gelen istekler (request) Controller’larda değerlendirilir, isteğin detayına göre hangi işlemlerin yapılacağı
+    ve kullanıcıya hangi View’ın döneceği (response) belirtilir.
+
+
+    Object Oriented katmanları nelerdir ?
+    4 temelden oluşur.
+    Soyutlama (Abstraction)
+    Kapsülleme (Encapsulation)
+    Miras Alma (Inheritance)
+    Çok Biçimlilik (Polymorphism)
+
+    ABSTRACTION: bir sınıfın özelliklerinin ve davranışlarının tanımlanmasına denir. Genellikle base sınıf olarak kullanılır.
+
+    ENCAPSULATION:Verinin, o veri üzerinde çalışacak metotlarla birlikte bir yapı altında toplanmasına encapsulation denir.soyutlamanın tasarlanmış hali olarakta düşünülebilir.
+    En basit haliyle sınıf da sarmalamanın gerçekleştirilmesi olarak düşünülebilir. Sarmalama ile nesnelerin veri yapıları ile davranışları, sınıf olarak mantıksal bir birim haline getirilir.
+    Sarmalamanın iç mekanızma dışardan saklanır.
+
+    İNHERİTANCE:Aralarında benzerlik bulunan sınıfları, dolayısıyla da nesneleri, bu benzerlik ile beraber oluşturmakta kullanılan kalıptır.
+    Extends anahtar kelimesi kullanılır miras yapısını kurmak için.
+    Çocuk sınıf, ebeveyninden devralır ve onu genişletir(extends). Javada çoklu miras yoktur.extends ten sonra bir tane sınıf gelebilir.
+
+    POLYMORPHİSM:Bir referansın, zamanın farklı anlarında, kendi ya da alt tipinden olan nesneleri gösterebilmesine denir.
+    Plymorphizim bir yaklaşımdır.İki referansın birbirleriyle haberleşip, birbirleriningerçek tiplerini bilmemeleri demektir.
+
+
+    SORU 2
+
+    REST ve SOAP mimarileri ile yapılır. Rest mimarisini kullanan servislere RESTful servis denir genelde.Client-server arasında ki veri alışverişini
+    SOAP, RPC gibi kompleks mimarilerle sağlamak yerine, HTTP protokolü üzerinden sağlamak. RESTful servisler SOAP, RPC’nin aksine basit ve hafiftirler.
+    Aynı zamanda oldukça esnektir. SOAP ile en büyük farkı, SOAP gibi bizi proxy kullanmaya, bir WSDL’e zorlamıyor olması.
+
+
+    Diğer sorular hakkında pek fazla bir bilgim yoktur.
+
+
+     */
+}
